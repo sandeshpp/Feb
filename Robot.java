907c0339a0bf1092b61bd19afe74e737d96d649f
sandeshpp <sandeshpandit@hotmail.com>
2017-03-17 11:17:23 -0500

    added a file robot.java

---
+++ Robot.java
@@ -0,0 +1,20 @@
+class Robot{
+
+public String read(String name){
+
+return name;
+}
+
+public static void main(String[] args){
+
+System.out.println("-------------");
+System.out.println("|  @     @  |");
+System.out.println("|     =     |");
+System.out.println("-------------");
+System.out.println("      |      ");
+System.out.println("    /   \\   ");
+System.out.println("      |      ");
+System.out.println("    /   \\   ");
+
+}
+}
